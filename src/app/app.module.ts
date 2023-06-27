1f21e226a60a489f53f4bdb2b999b5b94a6c4904
Hari Mahadevan <hari@smallpearl.com>
2023-06-28 00:28:14 +0800

    Add CrispyFormsConfig sample code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CrispyMatFormModule } from 'dist/crispy-mat-form';
+import {
+  CRISPY_FORMS_CONFIG_PROVIDER,
+  CrispyFormsConfig,
+  CrispyMatFormModule,
+} from 'dist/crispy-mat-form';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MyTelInput } from './components/my-tel-input/my-tel-input.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
+/**
+ * CrispyConfig demonstrator that converts all labels to uppercase.
+ */
+const CrispyConfig: CrispyFormsConfig = {
+  labelFn: (code: string) => code.toUpperCase(),
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -21,9 +30,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatButtonModule,
     MatFormFieldModule,
     MyTelInput,
-    CrispyMatFormModule
+    CrispyMatFormModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: CRISPY_FORMS_CONFIG_PROVIDER, useValue: CrispyConfig },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
